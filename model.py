84de9e43cd7bc642a77c3ffea912b78cf3c278d7
Muhammad Shoaib <shoaibidrees59@gmail.com>
2025-08-12 20:17:41 +0500

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,10 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import TreebankWordTokenizer
+import nltk
+
+# Download stopwords data if not already present
+nltk.download('stopwords')
 
 
 tokenizer = TreebankWordTokenizer()
